7e7d31972ea82ec18a3aaef0cf1036c06fd89f63
di3gojkj <diego.rivas.varas@gmail.com>
2025-07-10 02:17:01 -0400

    actualizacion de codigo de personas

--- Python20.py
+++ Python20.py
@@ -1,38 +1,42 @@
-personas={}
+personas = []
 
-def agregar_persona(nombre,mayor_edad):
-    personas[nombre]=[mayor_edad]
+def agregar_persona():
+    cantidad = int(input("\nIngrese la cantidad de personas que desea agregar: "))
+    for i in range(1, cantidad + 1):
+        print(f"\n-----------Guardar {i} personas--------------")
+        nombre = input("Ingrese el nombre de la persona: ").lower()
+        mayor_edad = input("¿Es mayor de edad? (Si/No): ").lower()
+        if mayor_edad == "si":
+            mayor_edad = True
+        elif mayor_edad == "no":
+            mayor_edad = False
+        
+        personas.append({
+            "nombre": nombre,
+            "mayor_edad": mayor_edad
+        })
 
-    for i,j in personas.items():
-        print("Persona :",i)
-        print("Mayor de edad :",j[0])
-
+def mostrar_persona():
+    if not personas:
+        print("No hay personas registradas con ese nombre.")
+    for indice, persona in enumerate(personas):
+        print(f"{indice+1} - {persona['nombre']} - {'Mayor de edad' if persona['mayor_edad'] else 'Menor de edad'}")
 
 while True:
-    print("*** Menu ***")
+    print("\n*** Menu ***")
     print("1. Agregar persona")
-    print("2. Mostrar una persona")
+    print("2. Mostrar personas")
     print("3. Salir")
 
     opcion = int(input("Ingrese una opcion : "))
 
-    if(opcion == 1):
-        
-        nombre_persona = input("Ingrese nombre persona : ")
-        
-        while True:
-            mayor_edad = input("¿Es mayor de edad? (S/N): ")
-            if mayor_edad.upper() == "S" or mayor_edad.upper() == "N":
-                break
-            else:
-             print("debe ingresar S o N solamente.")
-
-        agregar_persona(nombre_persona,mayor_edad)     
-
-    elif(opcion == 2):    
-        print("Opcion 1")
-    elif(opcion == 3):
-        print("Salir")
+    if opcion == 1:
+        agregar_persona()
+    elif opcion == 2:
+        mostrar_persona()
+    elif opcion == 3:
+        print("Saliendo del programa...")
         break
     else:
-        print("Opcion no valida")   
+        print("Opcion no valida, intente nuevamente.")
+        continue
